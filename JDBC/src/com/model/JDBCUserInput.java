e3bd5426140a2225aa7b6fbc6b94b5d2b54fb19e
vidyadhar003 <34331060+vidyadhar003@users.noreply.github.com>
2018-08-05 00:05:28 +0530

    Update JDBCUserInput.java

--- JDBC/src/com/model/JDBCUserInput.java
+++ JDBC/src/com/model/JDBCUserInput.java
@@ -22,7 +22,7 @@ public class JDBCUserInput {
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test");
 			
 			//This is test Java class
-			
+			//This is test Java class/This is test Java class
 			String sql="insert into customer values(?,?,?,?)";
 			
 			System.out.println("Enter product id");
